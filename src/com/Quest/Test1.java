303d81f75f280e346eaa50a0170000c53b69dca8
sahoo <sahoo@LAPTOP-6UITJDNN>
2021-03-01 12:00:52 +0530

    first commit

---
+++ src/com/Quest/Test1.java
@@ -0,0 +1,14 @@
+package com.Quest;
+
+public class Test1 {
+	public static void main(String[] args) {
+		String hungary="yes";
+		if(hungary=="yes")
+			System.out.println("eat something-------");
+		else {
+			System.out.println("Do Some Work----");
+		}
+			
+	}
+
+}
